3329d2b0fc7aff56c8d300684d41fb05b96a566d
Brian Maurice <briankinyua99@gmail.com>
2023-07-13 14:10:10 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /***
- * main - Printing \"programming is like building a multilingual
- * puzzle, followed by a new line.
+ * main - prints puts function
+ * 
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
