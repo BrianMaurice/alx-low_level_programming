6dc9fe911682b52a0bf42094054fac05791c15d1
Brian Maurice <briankinyua99@gmail.com>
2023-07-16 23:23:56 -0700

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char letters  = 'a';
+
+	while (letters <= 'z')
+	{
+		putchar(letters);
+
+		letters++;
+	}
+	putchar('\n');
+
+	return (0);
+}
