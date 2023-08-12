97765be97d11f7bcfba1d2aa028d10e11c5f144a
Brian Maurice <briankinyua99@gmail.com>
2023-08-12 13:02:14 -0700

    update

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
 	int result, i;
 
 	result = 0;
-	if (checker(argc, 1, 0, argv) == 1)
+	if (checker(argc, 1, 0, *argv[]) == 1)
 	{
 		printf("Error\n");
 		return (1);
